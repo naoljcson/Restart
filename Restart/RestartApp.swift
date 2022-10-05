799ae05fdf20c9697d9288ee7e03a89921242511
Naol Hussien <naol.h@suntecholding.com>
2022-10-05 15:34:07 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Naol Hassen on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
